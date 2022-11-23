472a3cdd80f37e34be9b659b7998ba6e96c3d72b
dediwibisono17 <dedi.wibisono17@gmail.com>
2022-11-23 11:01:22 +0700

    update

--- js/ceki.js
+++ js/ceki.js
@@ -17,4 +17,21 @@ gsap.registerPlugin(ScrollTrigger);
       },
     });
   }
+
+  const process2 = document.querySelector('.process2');
+  if ((typeof(process2) != 'undefined' && process2 != null)) {
+    let sections = gsap.utils.toArray('.process2__item');
+    gsap.to(sections, {
+      xPercent: -100 * (sections.length - 1),
+      ease: "none",
+      scrollTrigger: {
+        trigger: process2,
+        markers: false,
+        scrub: 1,
+        pin: true,
+        snap: 1 / (sections.length - 1),
+        end: () => "+=" + document.querySelector(".process2").offsetWidth
+      },
+    });
+  }
 }
