e93d4d824aab801dabd1bcef9a5861214101f367
dediwibisono17 <dedi.wibisono17@gmail.com>
2022-11-29 13:46:33 +0700

    updtae

--- js/ceki.js
+++ js/ceki.js
@@ -29,7 +29,7 @@ gsap.registerPlugin(ScrollTrigger);
         markers: false,
         scrub: 1,
         pin: true,
-        snap: 1 / (sections.length - 1),
+        // snap: 1 / (sections.length - 1),
         end: () => "+=" + document.querySelector(".process2").offsetWidth
       },
     });
